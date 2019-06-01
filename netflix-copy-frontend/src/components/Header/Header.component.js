f48b22becc7316511c6871be1d6b33102f9d8f08
emilio.heinzmann <emilio.heinzmann@cwi.com.br>
2019-06-01 12:37:00 -0300

    feat(profiles-home): Creating header component

---
+++ netflix-copy-frontend/src/components/Header/Header.component.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Header.styles.scss'
+
+const Header = () => {
+
+  return (
+    <div className='headerContainer'>
+      NETFLIX
+    </div>
+  )
+}
+
+export { Header }
