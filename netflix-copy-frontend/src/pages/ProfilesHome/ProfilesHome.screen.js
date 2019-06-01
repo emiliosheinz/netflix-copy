889631082e8942c101d52eadbb151da78aedbea1
emilio.heinzmann <emilio.heinzmann@cwi.com.br>
2019-06-01 17:42:51 -0300

    feat(profiles-home): Adding images to the profiles

--- netflix-copy-frontend/src/pages/ProfilesHome/ProfilesHome.screen.js
+++ netflix-copy-frontend/src/pages/ProfilesHome/ProfilesHome.screen.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { BlueProfile, RedProfile, YellowProfile, KidsProfile, GreenProfile } from '../../assets'
+
 import './ProfilesHome.styles.scss'
 
 const ProfilesHome = () => {
@@ -10,11 +12,26 @@ const ProfilesHome = () => {
         Quem está assistindo?
       </h1>
       <div className='profilesHome__profilesWrapper'>
-        <div className='profilesHome__profile'></div>
-        <div className='profilesHome__profile'></div>
-        <div className='profilesHome__profile'></div>
-        <div className='profilesHome__profile'></div>
-        <div className='profilesHome__profile'></div>
+        <div className='profilesHome__nameAndPic'>
+          <img src={GreenProfile} alt='Profile' className='profilesHome__profile' />
+          <span>Username</span>
+        </div>
+        <div className='profilesHome__nameAndPic'>
+          <img src={RedProfile} alt='Profile' className='profilesHome__profile' />
+          <span>Username</span>
+        </div>
+        <div className='profilesHome__nameAndPic'>
+          <img src={YellowProfile} alt='Profile' className='profilesHome__profile' />
+          <span>Username</span>
+        </div>
+        <div className='profilesHome__nameAndPic'>
+          <img src={BlueProfile} alt='Profile' className='profilesHome__profile' />
+          <span>Username</span>
+        </div>
+        <div className='profilesHome__nameAndPic'>
+          <img src={KidsProfile} alt='Profile' className='profilesHome__profile' />
+          <span>Kids</span>
+        </div>
       </div>
       <div className='profilesHome__buttonWrapper'>
         <button className='profilesHome__button'>GERENCIAR PERFIS</button>
